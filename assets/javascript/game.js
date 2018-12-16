f8ba7056c23d5916ce36920c7a95d137bf4f17c6
DaShinja <byron@dashinja.com>
2018-12-16 09:06:18 -0500

    fix(name): Change index.js name to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,218 +1,239 @@
 'use strict';
-// ID Dump
-// wins-text
-// loses-text
-// guessedLetters-text
-// guessesRemain-text
-// winOrLose-text
-// instruction-text
+// Variable Pieces
+const wordList = [
+  'Rojo',
+  'Amarillo',
+  'Azul',
+  'Verde',
+  'Naranja',
+  'Negro',
+  'Blanco',
+  'proscribir',
+  'delincuente',
+  'peligro',
+  'linchamiento',
+  'cuerda',
+  'ejecucion',
+  'justicia'
+];
 
-// Variable Dump
-// let userGuess = event.key;
-let keyPress;
-let keyPressCount = 0;
+const themeArray = [
+  'proscribir',
+  'delincuente',
+  'peligro',
+  'linchamiento',
+  'cuerda',
+  'ejecucion',
+  'justicia'
+]
+
+let correctLetterCount = 0;
 let wins = 0;
 let loses = 0;
+let theChosenOne;
+let chosenLetter;
+let wordBlanks = [];
 let guessedLetters = [];
-let guessesRemain;
-let wordReveal;
+let guessesRemain = 5;
+let instructions = 'Press any key to start';
 
-let wordList = ['Byron', 'Cedric', 'Dominoes'];
+// Word blank tools
+let wordBlanksText = document.getElementById('wordBlanks-text');
 
-// DOM write variables
-let wordRevealText = document.getElementById('wordBlanks-text');
+// Guessed letter tools
 let guessedLettersText = document.getElementById('guessedLetters-text');
+
+// Guess count tools
 let guessesRemainText = document.getElementById('guessesRemain-text');
-let winConditionText = document.getElementById('winOrLose-text');
+
+// Instruction tools
 let instructionText = document.getElementById('instruction-text');
 
-//condition to be used in if statement/////////////
-// const winCondition = wordReveal.includes('_') === false;
-// const failCondition = (guessesRemain === 0);
+// The Word Dev tool test
+// let theChosenOneTest = document.getElementById('theWordTest-text');
 
-// Game Start ///////////
-function startGame() {
-  //Resets
+let winsText = document.getElementById('wins-text');
+
+let losesText = document.getElementById('loses-text');
+
+function writeDOM() {
+  // write to DOM
+  guessedLettersText.textContent = guessedLetters.join(' ').toUpperCase()
+  guessesRemainText.textContent = guessesRemain;
+  instructionText.textContent = instructions;
+  // theChosenOneTest.textContent = theChosenOne; // <--- wut?
+  winsText.textContent = wins;
+  losesText.textContent = loses;
+}
+
+function writeWordBlanks(writables) {
+  wordBlanksText.textContent = writables.join(' ');
+}
+
+function start() {
+  // init start values
+  redo();
+  chooseWord();
+  makeWordBlanks();
+  writeWordBlanks(wordBlanks);
+  writeDOM();
+}
+
+function redo() { // <-- problems
+  // reset values
+  correctLetterCount = 0;
+  // theChosenOne;
+  // chosenLetter;
+  wordBlanks = [];
   guessedLetters = [];
   guessesRemain = 5;
-  guessesRemainText.textContent = guessesRemain;
-  instructionText.textContent =
-    'See the blanks? Press a letter to guess the word.';
+  instructions = 'Press any key to start';
+  //Function Resets
+  // chooseWord();
+  // writeWordBlanks(wordBlanks);
 
-  //Selects word from wordList
-  const wordBlankChoice = wordList[
+  // writeDOM();
+}
+
+function chooseWord() {
+  // choose word
+  theChosenOne = wordList[
     Math.floor(Math.random() * wordList.length)
-  ].toLowerCase(); //string
+  ].toLowerCase();
+   (theChosenOne);
+}
 
-  console.log(`chosen word: ${wordBlankChoice}`);
+function makeWordBlanks() {
+  theChosenOne.split('');
 
-  //Make Blanks for Chosen Word Appear
-  const wordBlanks = function() {
-    let holder = [];
-    for (let i = 0; i < wordBlankChoice.length; i++) {
-      holder.push(`_`);
+  for (let i = 0; i < theChosenOne.length; i++) {
+    wordBlanks.push('_');
+  }
+  // writeWordBlanks(wordBlanks)
+  //  (writeWordBlanks(wordBlanks));
+}
+
+document.onkeyup = function(event) {
+  // respond to key press
+  chosenLetter = event.key;
+
+  if (guessedLetters.includes(chosenLetter)) {
+    instructionText.textContent = `You already pressed ${chosenLetter}`;
+    guessesRemain--
+    writeDOM();
+    gameStatus()
+  } else {
+    writeDOM()
+    keyCheckTrue(chosenLetter);
+  }
+};
+
+function keyCheckTrue(letter) {
+  // key is in word
+  if (theChosenOne.includes(letter)) {
+     ('I check if true');
+    replaceLetter(letter);
+    gameStatus();
+  } else {
+    // key not in word
+     ("I'm letter before keyCheckFalse(): ", letter);
+    keyCheckFalse(letter);
+    gameStatus()
+  }
+}
+
+function keyCheckFalse(letter) {
+  // key not in word
+  // post to the DOM wrong letters
+
+  // DECREMENT guesses remaining
+  if (!theChosenOne.includes(letter) && guessesRemain !== 0) {
+    guessesRemain--;
+    //  ("I'm letter before the push: ", letter);
+    guessedLetters.push(letter);
+    
+    //  ('I am guessedLetters: ', guessedLetters);
+    writeDOM();
+  } else if (guessesRemain === 0) {
+    youLose();
+  }
+}
+
+function replaceLetter(letter) {
+  // show right letters in blanks
+
+  for (let i = 0; i < theChosenOne.length; i++) {
+    if (theChosenOne[i] === letter) {
+      wordBlanks[i] = letter;
+      correctLetterCount++;
+       (wordBlanks);
     }
-    let wordblanks = holder.join(' ');
-    return wordblanks; //string
-  };
+  }
+   ('correctLetterCount: ', correctLetterCount);
 
-  wordRevealText.textContent = wordBlanks();
-  console.log('call wordBlanks(): ' + wordBlanks());
+  writeWordBlanks(wordBlanks);
+  gameStatus();
+  //  (wordBlanks)
+  // test = letter
+  //  (test)
+}
 
-  //User Presses Key
-  //Word Selection && Display Blanks
+function gameStatus() {
+  switch (correctLetterCount === theChosenOne.length) {
+    case true:
+      youWin();
 
-  document.onkeyup = function(event) {
-    // console.log('I just pressed a key');
-    keyPress = event.key;
-    // console.log('keyPress freshly declared: ', keyPress);
-    keyPressCount++;
-    console.log(`keypress count ${keyPressCount}`);
+      break;
 
-    // if (!wordBlankChoice.includes(keyPress)) {
-    //   guessedLetters.push(keyPress);
-    //   guessedLettersText.textContent = guessedLetters;
-    // } else {
-    //   return;
-    // }
+    default:
+      instructions = 'Keep going!';
+  }
 
-    console.log('Pressed: ', keyPress);
-    // winConditionText.textContent = ' Game in Progress';
-    instructionText.textContent = 'Keep going! You can do it!';
-    if (wordBlankChoice.includes(keyPress) !== true) {
-      console.log(`turns out my word doesn't have "${keyPress}"`);
-      //decrement and report guesses remain
-      guessesRemain--;
-      guessesRemainText.textContent = guessesRemain;
+  if (guessesRemain < 1) {
+    youLose();
+  } else if (correctLetterCount > theChosenOne.length) {
+    youLose()
+  }
+}
 
-      //lose game
-      if (guessesRemain === 0) {
-        guessedLetters = [];
-        guessesRemain = 5;
-        guessesRemainText.textContent = guessesRemain;
-        instructionText.textContent =
-          'See the blanks? Press a letter to guess the word.';
+function youLose() {
+  // you lose
+  oneDown();
+}
 
-        //Selects word from wordList
-        const wordBlankChoice = wordList[
-          Math.floor(Math.random() * wordList.length)
-        ].toLowerCase(); //string
+function youWin() {
+  // you win
+  oneUp();
+}
 
-        console.log(`chosen word: ${wordBlankChoice}`);
+function oneUp() {
+  wins++;
+  instructionText.textContent = 'Great: You pass the turing test...sorta. Play more.';
+  // 5 seconds after winning, the game resets.
+  setTimeout(() => {
+   
+   start();
+ }, 5000);
 
-        //Make Blanks for Chosen Word Appear
-        const wordBlanks = function() {
-          let holder = [];
-          for (let i = 0; i < wordBlankChoice.length; i++) {
-            holder.push(`_`);
-          }
-          let wordblanks = holder.join(' ');
-          return wordblanks; //string
-        };
+}
 
-        wordRevealText.textContent = wordBlanks();
-        console.log('call wordBlanks(): ' + wordBlanks());
-      }
-    } else {
-      console.log('first hihihihihi');
-      // reveals wordblanks one by one
-      wordReveal = function() {
-        console.log("I'm wordReveal() I just started");
-        //checks for right letters
-        console.log(
-          "I'm wordBlankChoice: ",
-          wordBlankChoice.includes(keyPress)
-        );
-        if (wordBlankChoice.includes(keyPress)) {
-          console.log(
-            'My earlier key press matches a letter(s) in chosen word'
-          );
+function oneDown() {
+  loses++;
+  instructionText.textContent = 'Jerk! You lost to a bot. Bro... do you even Turing!?!?';
 
-          //string to array for use of filter()
-          let wordChoiceArray = wordBlankChoice.split('');
-          console.log('split wordChoiceArray: ' + wordChoiceArray); // does this need to change? (array no longer needed?)
-          console.log('hi first');
-          let wordHolder = function() {
-            // wordChoiceArray.map(keyPress => { //make me map instead? wordHolder is array keyPress=each index of wordChoiceArray
-            let place = wordBlankChoice.indexOf(keyPress); //place = num
-            let letterRevealerHolder = wordBlanks().split(''); //array
-            let letterRevealer = letterRevealerHolder.splice(
-              place,
-              1,
-              keyPress
-            ); // string
-            console.log('hi');
-            for (let i = 0; i < wordBlankChoice.length; i++) {
-              // let place = wordChoiceArray.indexOf(keyPress); //place = num
-              //   for (let i=0; i < wordChoiceArray.length; i++) {
-              console.log('place var: ' + place);
-              if (place !== -1) {
-                // = wordBlanks().wordblanks.split(',');
+  // alert('Jerk');
+  // $('#myModalLose').modal(show)
 
-                if (wordBlankChoice.includes(keyPress)) {
-                  wordBlanks().wordblanks[i] = wordBlankChoice.indexOf(
-                    keyPress
-                  );
-                  console.log('hilo');
-                }
-                console.log('letterRevealerHolder: ', letterRevealerHolder);
-                // let letterRevealer = wordBlanks().split(',').splice(place, 1, keyPress) // string
-                console.log('Just made letterRevealer: ', letterRevealer);
-                //PSEUDO:
-                /*
-              in Map:
-              operation = letterRevealerHolder
-              
-              */
+  // 5 seconds after winning, the game resets.
+  setTimeout(() => {
+    start();
+    
+  }, 5000);
 
-                // let letterRevealer = letterRevealerHolder.indexOf(keyPress).push()
 
-                // //to array
-                // .split(',')
-                // //finds place where letter matches blank, puts letter in place of blank
-                // .splice(place, 1, keyPress);
-                // console.log('I just spliced this: ' + letterRevealer);
+}
 
-                //to string
-                // letterRevealer.join(' ');
-                // console.log('place var: ' + place)
-                console.log('letterRevealer value: ' + letterRevealer);
-                return letterRevealer;
-              } else {
-                console.log('Error: Condition not met.');
-                return;
-              }
-            } // for loop ends
-          }; //wordHolder() end
-          // wordHolder();
-        } else {
-          // Writes to DOM guessed letters
-          if (guessedLetters.includes(keyPress)) {
-            instructionText.textContent = `You already pressed "${keyPress}"`;
-            // guessesRemainText.textContent = guessesRemain--
-          } else {
-            // Allows only wrong guess to post to guessed letters
-            guessedLetters.push(keyPress);
-            guessedLettersText.textContent = guessedLetters
-              .join(' ')
-              .toUpperCase()
-              .split(' ')
-              .join(' ');
-            console.log('guessed letters: ', guessedLetters);
-            console.log('guessedLettersText ', guessedLettersText.textContent);
-          }
-        }
-      }; wordReveal()
-    } //wordReveal() end;
-    wordReveal();
+//
 
-    ////////trash/////
-    // let wrongGuesses = guessedLetters.filter(letters => {
-    //   guessedLetters[letters] === wordBlankChoice[letters];
-    // });
-    // console.log('Processed wrongGuesses: ', wrongGuesses);
-    // wordReveal();
-  }; //onkeyup function end
-} //gameStart() end;
-
-startGame();
+//Start Game
+start();
