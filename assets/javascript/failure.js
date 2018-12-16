e18e209e178a590fce4fbf768e07056df2762a2e
DaShinja <byron@dashinja.com>
2018-12-15 23:12:41 -0500

    feat(diverse): Repair App + Add Bootstrap Theme

--- assets/javascript/failure.js
+++ assets/javascript/failure.js
@@ -10,6 +10,7 @@ const wordList = [
   'Blanco'
 ];
 
+let correctLetterCount = 0;
 let wins = 0;
 let loses = 0;
 let theChosenOne;
@@ -32,16 +33,24 @@ let guessesRemainText = document.getElementById('guessesRemain-text');
 let instructionText = document.getElementById('instruction-text');
 
 // The Word Dev tool test
-let theChosenOneTest = document.getElementById('theWordTest-text')
+let theChosenOneTest = document.getElementById('theWordTest-text');
 
+let winsText = document.getElementById('wins-text');
+
+let losesText = document.getElementById('loses-text');
 
 function writeDOM() {
   // write to DOM
-  wordBlanks.textContent = wordBlanks.join('');
-  guessedLettersText.textContent = guessedLetters.join('');
+  guessedLettersText.textContent = guessedLetters.join(' ').toUpperCase()
   guessesRemainText.textContent = guessesRemain;
   instructionText.textContent = instructions;
-  theChosenOneTest.textContent = theChosenOne
+  theChosenOneTest.textContent = theChosenOne; // <--- wut?
+  winsText.textContent = wins;
+  losesText.textContent = loses;
+}
+
+function writeWordBlanks(writables) {
+  wordBlanksText.textContent = writables.join(' ');
 }
 
 function start() {
@@ -49,82 +58,165 @@ function start() {
   redo();
   chooseWord();
   makeWordBlanks();
+  writeWordBlanks(wordBlanks);
+  writeDOM();
 }
 
 function redo() {
   // reset values
+  correctLetterCount = 0;
+  theChosenOne;
+  chosenLetter;
+  wordBlanks = [];
   guessedLetters = [];
   guessesRemain = 5;
   instructions = 'Press any key to start';
+  //Function Resets
+  chooseWord();
+  writeWordBlanks(wordBlanks);
+
+  // writeDOM();
 }
 
 function chooseWord() {
   // choose word
-  theChosenOne = wordList[Math.floor(Math.random() * wordList.length)];
+  theChosenOne = wordList[
+    Math.floor(Math.random() * wordList.length)
+  ].toLowerCase();
   console.log(theChosenOne);
 }
 
 function makeWordBlanks() {
-  theChosenOne.split('')
+  theChosenOne.split('');
 
-  for (let i=0; i < theChosenOne.length; i++) {
+  for (let i = 0; i < theChosenOne.length; i++) {
     wordBlanks.push('_');
   }
-  console.log(wordBlanks)
+  // writeWordBlanks(wordBlanks)
+  // console.log(writeWordBlanks(wordBlanks));
 }
 
 document.onkeyup = function(event) {
   // respond to key press
   chosenLetter = event.key;
-  console.log(chosenLetter)
-  keyCheckTrue(chosenLetter);
+
+  if (guessedLetters.includes(chosenLetter)) {
+    instructionText.textContent = `You already pressed ${chosenLetter}`;
+    guessesRemain--
+    writeDOM();
+    gameStatus()
+  } else {
+    writeDOM()
+    keyCheckTrue(chosenLetter);
+  }
 };
 
 function keyCheckTrue(letter) {
   // key is in word
   if (theChosenOne.includes(letter)) {
-    console.log('I check if true')
-    replaceLetter(letter)
+    console.log('I check if true');
+    replaceLetter(letter);
+    gameStatus();
+  } else {
+    // key not in word
+    console.log("I'm letter before keyCheckFalse(): ", letter);
+    keyCheckFalse(letter);
+    gameStatus()
   }
 }
 
-function keyCheckFalse() {
+function keyCheckFalse(letter) {
   // key not in word
+  // post to the DOM wrong letters
+
+  // DECREMENT guesses remaining
+  if (!theChosenOne.includes(letter) && guessesRemain !== 0) {
+    guessesRemain--;
+    // console.log("I'm letter before the push: ", letter);
+    guessedLetters.push(letter);
+    
+    // console.log('I am guessedLetters: ', guessedLetters);
+    writeDOM();
+  } else if (guessesRemain === 0) {
+    youLose();
+  }
 }
 
 function replaceLetter(letter) {
   // show right letters in blanks
-  // example: blankArray[i] = keyPress
-  let blankIndex = theChosenOne.indexOf(letter)
-  console.log(blankIndex)
-  wordBlanks[blankIndex] = letter 
-  console.log(wordBlanks)
+
+  for (let i = 0; i < theChosenOne.length; i++) {
+    if (theChosenOne[i] === letter) {
+      wordBlanks[i] = letter;
+      correctLetterCount++;
+      console.log(wordBlanks);
+    }
+  }
+  console.log('correctLetterCount: ', correctLetterCount);
+
+  writeWordBlanks(wordBlanks);
+  gameStatus();
+  // console.log(wordBlanks)
   // test = letter
   // console.log(test)
+}
+
+function gameStatus() {
+  switch (correctLetterCount === theChosenOne.length) {
+    case true:
+      youWin();
+
+      break;
+
+    default:
+      instructions = 'Keep going!';
+  }
+
+  if (guessesRemain < 1) {
+    youLose();
+  } else if (correctLetterCount > theChosenOne.length) {
+    youLose()
+  }
 }
 
 function youLose() {
   // you lose
   oneDown();
-  redo();
 }
 
 function youWin() {
   // you win
   oneUp();
-  redo()
 }
 
 function oneUp() {
   wins++;
+  instructionText.textContent = 'Great: You pass the turing test...sorta. Play more.';
+  // 5 seconds after winning, the game resets.
+  setTimeout(() => {
+   
+   redo();
+ }, 5000);
+
 }
 
 function oneDown() {
-  loses--;
+  loses++;
+  instructionText.textContent = 'Jerk! You lost to a bot. Bro... do you even Turing!?!?';
+
+  // alert('Jerk');
+  // $('#myModalLose').modal(show)
+
+  // 5 seconds after winning, the game resets.
+  setTimeout(() => {
+    redo();
+    
+  }, 5000);
+
+
 }
 
 //
 
 //Start Game
 start();
-
